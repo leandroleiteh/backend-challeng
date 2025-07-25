a19ff320d6d41344b6baadc598230d4ee531bac9
leandro <leandroluz201616@gmail.com>
2025-07-24 21:38:43 -0300

    test: add class for teste integration with jar

---
+++ src/test/java/com/leandroleiteh/integration/resources/JwtResourceIT.java
@@ -0,0 +1,6 @@
+package com.leandroleiteh.integration.resources;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class JwtResourceIT extends JwtResourceTest {}
