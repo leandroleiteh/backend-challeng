fa0128086f4320b78f3b1dd1dd72c58a6e10ff2b
leandro <leandroluz201616@gmail.com>
2025-07-25 01:16:14 -0300

    chore: add table for state terraform in bucket

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.0.0"
 
+  backend "s3" {
+    bucket         = "meu-terraform-state-backend"        # ajuste para o nome do seu bucket
+    key            = "backend-challenge/terraform.tfstate" # caminho dentro do bucket
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"                    # nome da sua tabela de locks
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
